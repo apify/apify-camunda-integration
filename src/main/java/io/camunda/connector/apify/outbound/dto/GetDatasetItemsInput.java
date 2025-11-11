2ee4c286cb00b67eade139384ced6d760dad02f0
Denny Nguyen <55693964+ngut37@users.noreply.github.com>
2025-11-11 17:29:50 +0100

    feat: add mvn clean -f /Users/hotdog/Projects/apify-camunda-integration/pom.xml

--- src/main/java/io/camunda/connector/apify/outbound/dto/GetDatasetItemsInput.java
+++ src/main/java/io/camunda/connector/apify/outbound/dto/GetDatasetItemsInput.java
@@ -1,9 +1,10 @@
 package io.camunda.connector.apify.outbound.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 
 public record GetDatasetItemsInput(
     @NotEmpty String datasetId,
-    Integer offset,
-    Integer limit
+    @Min(value = 0, message = "offset can't be negative") Integer offset,
+    @Min(value = 0, message = "limit can't be negative") Integer limit
 ) {}
