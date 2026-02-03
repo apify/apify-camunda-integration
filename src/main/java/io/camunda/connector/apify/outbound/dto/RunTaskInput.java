fc9d43df3f5d500dc0ff3feae704a3b3a717174d
David Omrai <david.omrai@seznam.cz>
2026-02-03 17:17:11 +0100

    fix: add validation for timeout input parameter

--- src/main/java/io/camunda/connector/apify/outbound/dto/RunTaskInput.java
+++ src/main/java/io/camunda/connector/apify/outbound/dto/RunTaskInput.java
@@ -2,12 +2,12 @@ package io.camunda.connector.apify.outbound.dto;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.validation.constraints.NotEmpty;
-
+import jakarta.validation.constraints.Min;
 
 public record RunTaskInput(
     @NotEmpty String taskId,
     JsonNode inputJson,
-    Integer timeout,
+    @Min(value = 0, message = "timeout can't be negative") Integer timeout,
     String memory,
     String buildTag,
     Boolean waitForFinish
